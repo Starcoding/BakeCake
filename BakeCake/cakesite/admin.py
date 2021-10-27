d1de0a6bac086885b64bc560484d8eaee8bb1e51
Shirlusha <sheerly.morra@gmail.com>
2021-10-27 17:28:46 +0300

    Add and register new model

--- BakeCake/cakesite/admin.py
+++ BakeCake/cakesite/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Component, CustomUser, Order
+
+admin.site.register(Component)
+admin.site.register(CustomUser)
+admin.site.register(Order)
 
 # Register your models here.
--- BakeCake/cakesite/models.py
+++ BakeCake/cakesite/models.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator
 from django.db import models
 from django.db.models.deletion import CASCADE
-from django.db.models.fields import BooleanField, CharField, IntegerField
+from django.db.models.fields import BooleanField, CharField, IntegerField, TextField, DateTimeField
 from django.db.models.fields.related import ForeignKey
-from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Component(models.Model):
@@ -18,7 +19,7 @@ class Component(models.Model):
         (TOPPING, 'Топпинг'),
         (BERRIES, 'Ягоды'),
         (DECORATION, 'Декор'),
-        
+
     ]
     name = CharField(
         'название',
@@ -40,6 +41,10 @@ class Component(models.Model):
         'Содержит сахар',
         default=False
     )
+
+    def __str__(self):
+        return f'{self.name}{self.price}'
+
     class Meta:
         verbose_name = 'Компонент'
         verbose_name_plural = 'Компоненты'
@@ -59,10 +64,28 @@ class CustomUser(AbstractUser):
         'Фамилия',
         max_length=64,
     )
+    phone_number = CharField(
+        verbose_name='Номер телефона',
+        max_length=20,
+        help_text='+79991234567',
+        default=''
+    )
+    clear_phone_number = PhoneNumberField(
+        verbose_name='Нормализованный номер телефона',
+        blank=True
+    )
+    social_id = CharField(
+        max_length=50,
+        verbose_name='Ссылка на соцсеть',
+        null=True,
+        blank=True)
     consent_with_pd = BooleanField(
         'Согласие на обработку персональных данных',
         default=False,
     )
+
+    def __str__(self):
+        return f'{self.first_name}{self.second_name}'
 
     class Meta:
         verbose_name = 'Пользователь'
@@ -70,14 +93,53 @@ class CustomUser(AbstractUser):
 
 
 class Order(models.Model):
-
     # добавить вид заказа: торт, капкейп и т.п. Но пока работаем только с тортом для МВП
 
     customer = ForeignKey(
         CustomUser,
         on_delete=CASCADE,
         related_name='orders',
-        verbose_name='Заказчик')
+        verbose_name='Заказчик'
+    )
+
+    lettering = TextField(
+        max_length=100,
+        verbose_name='Надпись',
+        help_text='Мы можем разместить на торте любую надпись, например: «С днем рождения!»',
+        null=True
+    )
+
+    delivery_date = DateTimeField(
+        verbose_name='Дата доставки',
+        blank=True,
+        auto_now=True
+    )
+
+    delivery_time = DateTimeField(
+        verbose_name='Время доставки',
+        blank=True,
+        auto_now=True
+    )
+
+    is_ordered = BooleanField(
+        verbose_name='Заказ сделан',
+        default=False
+    )
+
+    date_ordered = DateTimeField(
+        verbose_name='Дата создания заказа',
+        null=True
+    )
+
+    comment = TextField(
+        max_length=500,
+        verbose_name='Комментарий к заказу',
+        null=True)
+
+    promo_code = CharField(
+        max_length=15,
+        verbose_name='Промокод',
+        null=True)
 
     class Meta:
         verbose_name = 'Заказ'
