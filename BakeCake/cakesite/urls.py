018905c0ca2bc3fd9cbe6bfb69deca02b304f87f
Shirlusha <sheerly.morra@gmail.com>
2021-10-28 04:37:29 +0300

    Add template pages

--- BakeCake/cakesite/urls.py
+++ BakeCake/cakesite/urls.py
@@ -1,3 +1,10 @@
-urlpatterns = [
-    
-]
+from django.contrib import admin
+from django.conf import settings
+from django.urls import path, include
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [path('', views.show_cake, name='index'),
+               path('profile', views.show_profile, name='profile'),
+               path('signin', views.make_account, name='signin'),
+               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- BakeCake/cakesite/views.py
+++ BakeCake/cakesite/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Component, CustomUser, Order, Product
 
-# Create your views here.
+
+def show_cake(request):
+    components = Component.objects.all()
+    return render(request, 'index.html')
+
+
+def show_profile(request):
+    users = CustomUser.objects.all()
+    orders = Order.objects.all()
+    return render(request, 'profile.html')
+
+
+def get_signup(request):
+    users = CustomUser.objects.all()
+    return render(request, 'signup.html')
+
+
+def make_account(request):
+    return render(request, 'signin.html')
